38ae277451b4d2fbac37dbbf6055c1b5fee6098d
Thales Gelinger <thalesgelinger@gmail.com>
2024-06-14 21:48:31 -0300

    feat: update home render logs

--- views/home.go
+++ views/home.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"jazida-api/internal/infra/db"
+	"log/slog"
 	"net/http"
 )
 
@@ -10,11 +11,13 @@ type HomeProps struct {
 }
 
 func (v *ViewHandler) Home(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Render home page")
 	t := NewTemplate()
 
 	allLoads, err := v.db.GetLoads(r.Context())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		slog.Error("Error rendering home page", err)
 		return
 	}
 
